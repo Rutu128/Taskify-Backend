0659e0f879baad5f37f303f5ba734491a83f6cd9
Rutu <rutubhimani124@gmail.com>
2025-01-23 02:48:15 +0530

    Cookies setting Changed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,7 +32,7 @@ const login = async (req, res) => {
       expiresIn: "7d",
     });
     const options = {
-      httpOnly: false,
+      httpOnly: true,
       secure: true,
       sameSite: "strict",
       maxAge: 604800000,
